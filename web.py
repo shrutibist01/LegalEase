b88773e714f54be558e24a4d51259ce6caa158e8
shrutibist01 <shrutibist01@gmail.com>
2023-10-08 21:55:16 +0000

    changes1

--- web.py
+++ web.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 web = Flask(__name__)
 
+
 @web.route('/')
 def hello_LegalEase():
-  return render_template('home.html',company_name='LegalEase')
+  return render_template('home.html', company_name='LegalEase')
+
 
 if __name__ == "__main__":
   web.run(host="0.0.0.0", debug=True)
